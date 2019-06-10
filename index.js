7ed1b9bd470e043e62ac605688ce95eb34ca6248
Nilesh Patva <nileshpatva@gmail.com>
2019-06-11 00:40:36 +0530

    Added example of How to create a mail and send it over SMTP

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // TO-DO: Write code here to send mails using nodemailer package
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass // generated ethereal password
+    }
+  });
+
+  // Create mail with below properties
+  const options = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "email@example.com, email2@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+    attachments: [
+      {
+        filename: "Packages file"
+      },
+      {
+        path: "package.json"
+      }
+    ]
+  };
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(options);
+
+  console.log("Message sent: %s", info.messageId);
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+main().catch(console.error);
